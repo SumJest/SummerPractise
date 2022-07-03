a2387912bbcdd9e9b734f701262f57798d912a89
SumJest <summedjesters@gmail.com>
2022-07-04 02:14:23 +0500

    change filename formatting

--- filemanager.py
+++ filemanager.py
@@ -2,23 +2,23 @@ import os
 
 
 class FileManager:
-    __dirs__ = ["logs"]
+    __dirs = ["logs"]
 
-    __files__ = {"input": "input.txt", "output": "output.txt", "log_translator": "logs/translator.log",
+    __files = {"input": "input.txt", "output": "output.txt", "log_translator": "logs/translator.log",
                  "log_syntax": "logs/syntax.log", "log_lexical": "logs/lexical.log"}
 
-    def __checkpaths__(self):
-        for dir in self.__dirs__:
+    def _checkpaths(self):
+        for dir in self.__dirs:
             if not os.path.exists(dir):
                 os.makedirs(dir)
-        for file in self.__files__.values():
+        for file in self.__files.values():
             if not os.path.exists(file):
                 with open(file, 'w') as f_io:
                     f_io.close()
 
-    def __clearlogs__(self):
-        for log_file_key in filter(lambda x: "log_" in x, self.__files__.keys()):
-            with open(self.__files__[log_file_key], "w") as f_io:
+    def _clearlogs(self):
+        for log_file_key in filter(lambda x: "log_" in x, self.__files.keys()):
+            with open(self.__files[log_file_key], "w") as f_io:
                 f_io.write("")
                 f_io.close()
 
@@ -27,7 +27,7 @@ class FileManager:
         Function returns data from input file
         :return: str
         """
-        with open(self.__files__['input'], 'r') as f_io:
+        with open(self.__files['input'], 'r') as f_io:
             input_data = f_io.read()
             f_io.close()
         return input_data
@@ -37,7 +37,7 @@ class FileManager:
         Function saves data to output file
         :return:
         """
-        with open(self.__files__['output'], 'w') as f_io:
+        with open(self.__files['output'], 'w') as f_io:
             f_io.write(data)
             f_io.close()
 
@@ -48,15 +48,15 @@ class FileManager:
         :param log_message: str
         :return:
         """
-        if f"log_{blockname}" not in self.__files__.keys():
+        if f"log_{blockname}" not in self.__files.keys():
             print(f"Not found log file for {blockname}.")
             return
-        log_file = self.__files__[f"log_{blockname.rstrip('.py')}"]
+        log_file = self.__files[f"log_{blockname.rstrip('.py')}"]
 
         with open(log_file, 'a') as f_io:
             f_io.write(log_message + "\n")
             f_io.close()
 
     def __init__(self):
-        self.__checkpaths__()
-        self.__clearlogs__()
+        self._checkpaths()
+        self._clearlogs()
